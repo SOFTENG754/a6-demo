ab44e5a9c72817ba5560b4eca866250094047615
dana <eksk325@gmail.com>
2024-06-04 18:57:35 +1200

    refactor course category page

--- src/test/java/nz/ac/auckland/se754/web/pages/CourseCategoryPage.java
+++ src/test/java/nz/ac/auckland/se754/web/pages/CourseCategoryPage.java
@@ -1,14 +1,23 @@
 package nz.ac.auckland.se754.web.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public class CourseCategoryPage {
+
+    @FindBy(className = "category-name")
+    private List<WebElement> categoryNames;
     public CourseCategoryPage(WebDriver driver) {
         PageFactory.initElements(driver, this);
     }
 
     public String[] getCategories() {
-        return new String[]{};
+        return categoryNames.stream()
+                .map(WebElement::getText)
+                .toArray(String[]::new);
     }
 }
